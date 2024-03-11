2e75d8f750671e3df6d09a8e23444971b6ddca1c
CojaONIX <cojaonix@gmail.com>
2024-03-11 20:06:27 +0100

    route nav

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('home.view') }}">Home</a>
+        <a class="navbar-brand" href="/">ONIX</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -30,13 +30,13 @@
                 <div class="nav-item dropdown ms-3">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Hello, {{ Auth::user()->name}}</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>
-                        <li><a class="dropdown-item" href="/profile">Profile</a></li>
+{{--                        <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>--}}
+{{--                        <li><a class="dropdown-item" href="/profile">Profile</a></li>--}}
 
                         <li><hr class="dropdown-divider"></li>
 
                         <li>
-                            <form method="post" action="/logout" class="dropdown-item">
+                            <form method="post" action="{{ route('logout') }}" class="dropdown-item">
                                 @csrf
                                 <button class="btn btn-outline-danger col-12" type="submit">Logout</button>
                             </form>
